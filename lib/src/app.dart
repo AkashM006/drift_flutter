380017ecfd294f3fbfd825d383e718a523dbe8a8
Akash M <akashm006@outlook.com>
2024-10-22 15:40:40 +0530

    Orientation lock setup done

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,6 +1,7 @@
 import 'package:drift_flutter/src/core/constants/size.dart';
 import 'package:drift_flutter/src/routing/router.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -8,6 +9,11 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     SizeConfig.init(context);
+
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
 
     return MaterialApp.router(
       routerConfig: router,

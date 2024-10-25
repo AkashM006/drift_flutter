8c4a5554ec38c118c572c9970248815ade6f25ae
Akash M <akashm006@outlook.com>
2024-10-25 13:16:12 +0530

    Domain integration for new task complete

--- lib/src/features/tasks/domain/usecase/add_task.usecase.dart
+++ lib/src/features/tasks/domain/usecase/add_task.usecase.dart
@@ -1,11 +1,15 @@
 import 'package:drift_flutter/src/core/usecase/usecase.dart';
 import 'package:drift_flutter/src/core/utils/data_state.util.dart';
 import 'package:drift_flutter/src/features/tasks/domain/entity/task.entity.dart';
+import 'package:drift_flutter/src/features/tasks/domain/repository/task.repository.dart';
 
 class AddTaskUsecase extends Usecase<Future<DataState<void>>, TaskEntity> {
+  final TaskRepository _taskRepository;
+
+  AddTaskUsecase(this._taskRepository);
+
   @override
-  Future<DataState<void>> call({required TaskEntity params}) {
-    // TODO: implement call
-    throw UnimplementedError();
+  Future<DataState<void>> call({required TaskEntity params}) async {
+    return await _taskRepository.addTask(params);
   }
 }

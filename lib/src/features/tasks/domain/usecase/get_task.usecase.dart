e816fde4a8d194ce7645146e346e105d0b5efc09
Akash M <akashm006@outlook.com>
2024-11-11 15:35:51 +0530

    Domain layer done for task detail

--- lib/src/features/tasks/domain/usecase/get_task.usecase.dart
+++ lib/src/features/tasks/domain/usecase/get_task.usecase.dart
@@ -1,11 +1,15 @@
 import 'package:drift_flutter/src/core/usecase/usecase.dart';
 import 'package:drift_flutter/src/core/utils/data_state.util.dart';
 import 'package:drift_flutter/src/features/tasks/domain/entity/task.entity.dart';
+import 'package:drift_flutter/src/features/tasks/domain/repository/task.repository.dart';
 
 class GetTaskUsecase extends Usecase<Future<DataState<TaskEntity>>, int> {
+  final TaskRepository _taskRepository;
+
+  GetTaskUsecase(this._taskRepository);
+
   @override
   Future<DataState<TaskEntity>> call({required int params}) {
-    // TODO: implement call
-    throw UnimplementedError();
+    return _taskRepository.getTask(params);
   }
 }

40198c314064a9ba9a7a753d96b076f8094a27a1
Akash M <akashm006@outlook.com>
2024-11-11 16:32:56 +0530

    Task update domain complete

--- lib/src/features/tasks/domain/usecase/edit_task.usecase.dart
+++ lib/src/features/tasks/domain/usecase/edit_task.usecase.dart
@@ -1,11 +1,15 @@
 import 'package:drift_flutter/src/core/usecase/usecase.dart';
 import 'package:drift_flutter/src/core/utils/data_state.util.dart';
 import 'package:drift_flutter/src/features/tasks/domain/entity/task.entity.dart';
+import 'package:drift_flutter/src/features/tasks/domain/repository/task.repository.dart';
 
 class EditTaskUsecase extends Usecase<Future<DataState<void>>, TaskEntity> {
+  final TaskRepository _taskRepository;
+
+  EditTaskUsecase(this._taskRepository);
+
   @override
   Future<DataState<void>> call({required TaskEntity params}) {
-    // TODO: implement call
-    throw UnimplementedError();
+    return _taskRepository.editTask(params);
   }
 }

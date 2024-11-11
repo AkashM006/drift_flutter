b648367a2264ea6e490b7187250b8c885af588b8
Akash M <akashm006@outlook.com>
2024-11-11 16:36:51 +0530

    Task update provider done

---
+++ lib/src/features/tasks/presentation/providers/edit_task/edit_task.provider.dart
@@ -0,0 +1,30 @@
+import 'package:drift_flutter/src/core/utils/data_state.util.dart';
+import 'package:drift_flutter/src/features/tasks/data/repository/task_impl.repository.dart';
+import 'package:drift_flutter/src/features/tasks/domain/usecase/edit_task.usecase.dart';
+import 'package:drift_flutter/src/features/tasks/presentation/dto/task.dto.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'edit_task.provider.g.dart';
+
+@riverpod
+class EditTask extends _$EditTask {
+  @override
+  DataState<String>? build() {
+    return null;
+  }
+
+  void go(TaskDto newTask) async {
+    final taskRepository = ref.read(taskImplProvider);
+
+    state = DataLoading();
+
+    final result = await EditTaskUsecase(taskRepository)(
+      params: newTask.toEntity(),
+    );
+
+    state = result.fold(
+      onSuccess: (data) => DataSuccess("Updated your tasks successfully!"),
+      onFailure: (error) => DataFailure(error),
+    );
+  }
+}
---
+++ lib/src/features/tasks/presentation/providers/edit_task/edit_task.provider.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'edit_task.provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$editTaskHash() => r'983bb58b9fa7e04d2e5d5b08d187a34c56cfb047';
+
+/// See also [EditTask].
+@ProviderFor(EditTask)
+final editTaskProvider =
+    AutoDisposeNotifierProvider<EditTask, DataState<String>?>.internal(
+  EditTask.new,
+  name: r'editTaskProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$editTaskHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$EditTask = AutoDisposeNotifier<DataState<String>?>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member

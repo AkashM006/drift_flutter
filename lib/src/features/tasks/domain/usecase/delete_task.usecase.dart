3b547eb8a860067c3d3cf7f5c7ba63ff0e388599
Akash M <akashm006@outlook.com>
2024-11-12 11:30:24 +0530

    Domain layer complete for task delete

--- lib/src/features/tasks/domain/usecase/delete_task.usecase.dart
+++ lib/src/features/tasks/domain/usecase/delete_task.usecase.dart
@@ -1,10 +1,14 @@
 import 'package:drift_flutter/src/core/usecase/usecase.dart';
 import 'package:drift_flutter/src/core/utils/data_state.util.dart';
+import 'package:drift_flutter/src/features/tasks/domain/repository/task.repository.dart';
 
 class DeleteTaskUsecase extends Usecase<Future<DataState<void>>, int> {
+  final TaskRepository _taskRepository;
+
+  DeleteTaskUsecase(this._taskRepository);
+
   @override
   Future<DataState<void>> call({required int params}) {
-    // TODO: implement call
-    throw UnimplementedError();
+    return _taskRepository.deleteTask(params);
   }
 }
